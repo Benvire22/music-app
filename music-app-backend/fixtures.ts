c897ac7b8aa9ca7d378398af8a0a726882126000
Limarev Ivan <ivantaketo@gmail.com>
2024-09-13 19:00:16 +0600

    seed fixtures.ts

--- music-app-backend/fixtures.ts
+++ music-app-backend/fixtures.ts
@@ -28,7 +28,7 @@ const run = async () => {
       photo: 'fixtures/john.jpg',
   });
 
-  const [summerAlbum, springAlbum, darknessAlbum] = await Album.create({
+  const [summerAlbum, springAlbum, darknessAlbum, paradiseAlbum] = await Album.create({
       name: 'Summer',
       artist: ivanArtist,
       releaseDate: 2024,
@@ -42,6 +42,11 @@ const run = async () => {
       name: 'Darkness',
       artist: sergeyArtist,
       releaseDate: 2018,
+      image: 'fixtures/darkness.webp',
+  },{
+      name: 'Paradise',
+      artist: sergeyArtist,
+      releaseDate: 2012,
       image: 'fixtures/darkness.webp',
   });
 
@@ -70,7 +75,82 @@ const run = async () => {
       album: darknessAlbum,
       length: '2:50',
       number: 3,
-  });
+  },{
+    name: 'Wrong heaven',
+    album: paradiseAlbum,
+    length: '5:15',
+    number: 1,
+  }, {
+    name: 'Hot wheels',
+    album: summerAlbum,
+    length: '5:30',
+    number: 2,
+  }, {
+    name: 'Dont cry',
+    album: paradiseAlbum,
+    length: '3:33',
+    number: 2,
+  }, {
+    name: 'Spring is coming!',
+    album: springAlbum,
+    length: '7:20',
+    number: 2,
+  }, {
+    name: 'Not over',
+    album: darknessAlbum,
+    length: '6:32',
+    number: 4,
+  },{
+    name: 'Believe me',
+    album: paradiseAlbum,
+    length: '2:15',
+    number: 3,
+  }, {
+    name: 'Hey! hey! hey everyone!',
+    album: summerAlbum,
+    length: '3:35',
+    number: 3,
+  }, {
+    name: "Don't worry",
+    album: darknessAlbum,
+    length: '5:32',
+    number: 5,
+  }, {
+    name: "It's time to shine!",
+    album: springAlbum,
+    length: '3:40',
+    number: 3,
+  }, {
+    name: 'It is everlasting Summer',
+    album: summerAlbum,
+    length: '3:20',
+    number: 5,
+  },{
+    name: 'I am Here',
+    album: paradiseAlbum,
+    length: '2:15',
+    number: 4,
+  }, {
+    name: "You hear about it, didn't you?",
+    album: springAlbum,
+    length: '5:20',
+    number: 4,
+  }, {
+    name: 'falling',
+    album: paradiseAlbum,
+    length: '2:32',
+    number: 5,
+  }, {
+    name: 'Cherry!',
+    album: springAlbum,
+    length: '5:53',
+    number: 5,
+  }, {
+    name: 'Fine',
+    album: summerAlbum,
+    length: '3:23',
+    number: 5,
+  },);
 
   await db.close();
   await mongoose.disconnect();
